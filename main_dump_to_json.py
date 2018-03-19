9a50c525f5ca841feb4faa823df6ab6fba8591a8
Artemii Bykov <theunsignedfairon@gmail.com>
2018-03-19 15:13:50 +0300

    Cleaning main_dump_to_json file

--- main_dump_to_json.py
+++ main_dump_to_json.py
@@ -4,21 +4,3 @@ json_file = open('json_temp_db.json', 'w')
 
 event_id = 'RE201'
 client_id = 'C3FE7A81D2B94946F9F673E2A6A2D243'
-
-article_ids_parser = AcmDlListOfProceedingsParser.AcmDlListOfProceedingsParser()
-
-json_file.write('[\n')
-
-article_ids = article_ids_parser.parse(event_id, client_id)
-
-
-for article_id in article_ids[:5]:
-    print('Current: {0}/{1}'.format(article_ids.index(article_id) + 1, len(article_ids)))
-    try:
-        article_data = json.dumps(AcmDlArticleParser.AcmDlArticleParser().parse(article_id), indent=4)
-    except:
-        continue
-    json_file.write(article_data)
-    json_file.write('\n,')
-    print('#' * 100)
-json_file.write(']\n')
