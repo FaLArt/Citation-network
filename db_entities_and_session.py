0f517e46f4754f0f46083a6bbff3e118d0b51408
Artemii Bykov <theunsignedfairon@gmail.com>
2018-03-20 23:32:51 +0300

    Working parser

--- db_entities_and_session.py
+++ db_entities_and_session.py
@@ -8,14 +8,15 @@ db.bind(provider='mysql', host='localhost', user='root', passwd='root', db='soc_
 
 class Venue(db.Entity):
     venue_id = PrimaryKey(int, auto=True)
-    name = Required(str, unique=True)
+    name = Required(unicode, unique=True)
+    url = Optional(unicode, max_len=256)
     venue__article_venue_id = Set('Article')
 
 
 class Article(db.Entity):
     article_id = PrimaryKey(int)
-    title = Required(str)
-    abstract = Required(str, max_len=65000)
+    title = Required(unicode)
+    abstract = Required(LongUnicode)
     year = Required(int)
     venue_id = Optional(Venue)
     article__citation_article_id = Set('Citation', reverse='article_id')
@@ -32,8 +33,8 @@ class Citation(db.Entity):
 
 class Author(db.Entity):
     author_id = PrimaryKey(int, auto=True)
-    name = Required(str, unique=True)
-    url = Optional(str, max_len=256)
+    name = Required(LongUnicode)
+    url = Optional(unicode, max_len=256)
     author__author_article = Set('AuthorArticle', reverse='author_id')
 
 
